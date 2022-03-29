82c1d8b8976688c22f15390689951d24fc4ab284
Martijn <12066560+Kipjr@users.noreply.github.com>
2022-03-29 08:11:33 +0200

    fix slash

--- update.ps1
+++ update.ps1
@@ -13,9 +13,9 @@ cd $basedir
 git config --global --unset core.autocrlf
 
 #build
-if(-not(test-path -path "$basedir\spigot-buildtools")) { new-item -itemtype directory -path "$basedir" -name "spigot-buildtools" }
-cd "$basedir\spigot-buildtools"
-curl -o "$basedir\spigot-buildtools\BuildTools.jar" https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+if(-not(test-path -path "$basedir/spigot-buildtools")) { new-item -itemtype directory -path "$basedir" -name "spigot-buildtools" }
+cd "$basedir/spigot-buildtools"
+curl -o "BuildTools.jar" https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 
 #get latest version
 $ResponseVersions = Invoke-RestMethod -Method get -Uri  https://launchermeta.mojang.com/mc/game/version_manifest.json
@@ -25,18 +25,19 @@ $LatestVersion= $ResponseVersions.latest.release
 # $ServerJarURL=$ResponseLatest.downloads.server.url #not needed
 
 #download latest version
+cd $basedir
 $previousVersion = get-childitem $basedir -Directory "minecraft-*" | sort -Property LastWriteTime -Descending | select -first 1 -expandproperty FullName #D:\Downloads\minecraft-1.18
-if(-not(test-path -path "$basedir\minecraft-$LatestVersion")) { new-item -itemtype directory -path "$basedir" -name "minecraft-$LatestVersion" }
+if(-not(test-path -path "$basedir/minecraft-$LatestVersion")) { new-item -itemtype directory -path "$basedir" -name "minecraft-$LatestVersion" }
 # curl -o "$basedir/$LatestVersion/server-$LatestVersion.jar" $ServerJarURL #not needed
 
 #execute buildtools
-& $java -jar "$basedir\spigot-buildtools\BuildTools.jar" --rev $LatestVersion
-Remove-Item -path "$basedir\spigot-buildtools\*" -Exclude "*.jar" -Recurse -Force
+& $java -jar "$basedir/spigot-buildtools/BuildTools.jar" --rev $LatestVersion
+Remove-Item -path "$basedir/spigot-buildtools/*" -Exclude "*.jar" -Recurse -Force
 # spigot-1.18.2.jar
 # BuildTools.jar
 
-copy-item  "$previousVersion" "$basedir\minecraft-$LatestVersion\"
-remove-item "$basedir\minecraft-$LatestVersion\spigot-*.jar"
-$serverproperties = get-content -raw "$basedir\minecraft-$LatestVersion\server.properties"
-$serverproperties -replace 'motd=The (.*?) Server', "motd=The $LastestVersion Server" | set-content "$basedir\minecraft-$LatestVersion\server.properties"
-move-item  "$basedir\spigot-buildtools\spigot-$LatestVersion.jar" "$basedir\minecraft-$LatestVersion\"
+copy-item  "$previousVersion" "$basedir/minecraft-$LatestVersion/"
+remove-item "$basedir/minecraft-$LatestVersion/spigot-*.jar"
+$serverproperties = get-content -raw "$basedir/minecraft-$LatestVersion/server.properties"
+$serverproperties -replace 'motd=The (.*?) Server', "motd=The $LastestVersion Server" | set-content "$basedir/minecraft-$LatestVersion/server.properties"
+move-item  "$basedir/spigot-buildtools/spigot-$LatestVersion.jar" "$basedir/minecraft-$LatestVersion/”
